1dad18b6160a16b78cf38b6cb3a669cb14c5e9d4
Giovanni Peirone <mgpeirone25@gmail.com>
2025-06-01 13:40:59 -0300

    😧 :} 💯

---
+++ practicas/a+b/giovanni/a_mas_b.java
@@ -0,0 +1,14 @@
+
+
+public class a_mas_b{
+    
+    public static int a_mas_b(int a, int b) {
+        return a + b;
+    } 
+    
+    public static void main(String[] args){
+        System.out.println(a_mas_b(10, 10));
+    }
+}
+
+
